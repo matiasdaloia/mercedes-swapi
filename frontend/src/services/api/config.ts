0f4570c40e296dbc1a1720ecda8095fcd2dda403
Matias Daloia <matidaloia@gmail.com>
2025-07-18 15:47:43 +0200

    fix: docker setup

--- frontend/src/services/api/config.ts
+++ frontend/src/services/api/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
   endpoints: {
     people: "/people",
     planets: "/planets",
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,25 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
+    optimizeDeps: {
+      exclude: ["lucide-react"],
+    },
+    define: {
+      VITE_API_BASE_URL: JSON.stringify(`${env.VITE_API_BASE_URL}`),
+    },
+  };
 });
 
